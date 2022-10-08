fe9dd649b21d4dccdc5fc2476ce5473788730b9f
matt <myounger009@gmail.com>
2022-10-08 16:15:28 +0100

    Write CMakeLists.txt

---
+++ source/main.cxx
@@ -0,0 +1,19 @@
+// N Pendulum simulation
+
+#include <types/type_alias.hxx>
+
+class Pendulum {
+  Pendulum();
+
+  A();
+
+  b();
+
+  f();
+
+  RK4();
+
+  step();
+
+  vec<vec2> get_coords();
+};
